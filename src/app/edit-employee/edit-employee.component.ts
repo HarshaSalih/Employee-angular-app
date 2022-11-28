63a61bb7cebd1f2f35784e0b21477d89f9b0bb0b
HarshaSalih <harshasalih2000@gmail.com>
2022-11-28 21:39:01 +0530

    commits

--- src/app/edit-employee/edit-employee.component.ts
+++ src/app/edit-employee/edit-employee.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class EditEmployeeComponent {
 
+  code=""
+
+  readValues=()=>
+  {
+    let data:any={"code":this.code}
+    console.log(data)
+  }
+
 }
